d03688515a0fba7fadd7d99ffa33056853ae26aa
puthusseri <vyshakputhusseri@gmail.com>
2018-11-20 16:54:40 +0530

    Added the charactercontroller in script

--- Assets/Scripts/VRPlayer_Movement.cs
+++ Assets/Scripts/VRPlayer_Movement.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VRPlayer_Movement : MonoBehaviour {
-
+	
+	public Transform vrCamera;
+    public float toggleAngle = 30.0f;
+    public float speed = 3.0f;
+    public bool moveForward;
+	private CharacterController cc;
+	
 	// Use this for initialization
 	void Start () {
 		
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
