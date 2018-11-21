635c8c83ec4e361b4634b0ae8d3b173126b7c038
puthusseri <vyshakputhusseri@gmail.com>
2018-11-21 19:30:38 +0530

    modified the signboard to clikable , but script was not attached.

---
+++ Assets/Materials/Spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	// Use this for initialization
+	public float spinValue =90;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3 .up * spinValue * Time.deltaTime);
+	}
+	public void flipSpin() {
+		spinValue = -spinValue;
+	}
+}
