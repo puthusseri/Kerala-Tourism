bcd8aadc375d8045274ab65217af95d1313b6fb0
puthusseri <vyshakputhusseri@gmail.com>
2019-02-02 17:06:30 +0530

    added the functionality of scene back in video sphere

---
+++ Assets/Scripts/ScreenBackControllere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScreenBackControllere : MonoBehaviour {
+
+	// Use this for initialization
+	int screenIndex;
+	void Start () {
+		screenIndex = SceneManager.GetActiveScene().buildIndex;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene( screenIndex - 1 );
+		}
+	}
+}
